9ebc865c2ac29c588169a0e4db682e68eee10c96
Max-Rus1977 <maxsvet77@yandex.ru>
2021-10-10 19:50:03 +0300

    my hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
+import { usePost } from "./components/hooks/usePost";
 
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/modal/MyModal";
@@ -10,26 +11,11 @@ import PostForm from "./components/PostForm";
 import "./stiles/App.css";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Заголовок первого поста', body: 'Какой-то интересный текст' },
-    { id: 2, title: 'Заголовок второго', body: 'Произвольный текст' },
-    { id: 3, title: 'Третий заголовок', body: 'Супер интересный пост' },
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
 
-  const sortedPost = useMemo(() => {
-
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPost])
+  const sortedAndSearchPosts = usePost(posts, filter.sort, filter.query);
 
   function createPost(newPost) {
     setPosts([...posts, newPost])
---
+++ src/components/hooks/usePost.js
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+
+export const useSortedPost = (posts, sort) => {
+
+  const sortedPost = useMemo(() => {
+
+    if (sort) {
+      return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]));
+    }
+    return posts;
+  }, [sort, posts]);
+
+  return sortedPost;
+}
+
+export const usePost = (posts, sort, query) => {
+
+  const sortedPost = useSortedPost(posts, sort);
+
+  const sortedAndSearchPosts = useMemo(() => {
+    return sortedPost.filter(post => post.title.toLowerCase().includes(query))
+  }, [query, sortedPost])
+
+  return sortedAndSearchPosts;
+
+}
